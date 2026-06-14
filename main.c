248ff7d40bcede18b357b502dc2dcf0e2aeb348e
Yuan Xu <yuanx02122010@gmail.com>
2026-06-14 13:51:02 -0700

    First commit

---
+++ main.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include "main.h"
+
+#include <stdlib.h>
+
+int main(void) {
+    int num = 20;
+    struct HashMap hashMap = makeHashMap(num);
+    for (int i = 0; i < num; i++) {
+        addNode(i, (int)(rand() * 10), &hashMap);
+    }
+    //struct Result r = getValue(5, &hashMap);
+    for (int i = 0; i < hashMap.numBuckets; i++) {
+        printBucket(&hashMap, i);
+    }
+    printf("The code has reached this Yay");
+    // if (r.foundValue) {
+    //     printf("The value is %d.", r.value);
+    //     return 0;
+    // }
+    // printf("The value was not found.");
+    // return 1;
+}
+
+struct HashMap makeHashMap(int num) {
+    struct HashMap hashMap;
+    hashMap.numNodes = num;
+    hashMap.numBuckets = num / 3;
+    hashMap.size = sizeof(struct Node *) * hashMap.numBuckets +
+        sizeof(struct Node) * hashMap.numNodes;
+    hashMap.buffer = malloc(hashMap.size);
+    hashMap.unalloc = hashMap.buffer + sizeof(struct Node *) * hashMap.numBuckets;
+    
+    // struct Node ** because there are pointers being stored, therefore we need pointers to those pointers
+    for (int i = 0; i < hashMap.numBuckets; i++) {
+        ((struct Node **) (hashMap.buffer))[i] = NULL;
+    }
+
+    return hashMap;
+}
+
+int addNode(int key, int val, struct HashMap *hashMap) {
+    struct Node node;
+    node.key = key;
+    node.value = val;
+    node.next = NULL;
+    // Add the size of the buffer to the start of the buffer and ensure
+    // that it is greater than the unallocated slot with an additional space
+    if (hashMap->buffer + hashMap->size > (void *)(hashMap->unalloc + 1)) {
+        *(hashMap -> unalloc) = node;
+        hashMap->unalloc++;
+        struct Node *currentNode = hashMap->unalloc;
+        // ADDING THE NODE TO A BUCKET
+        struct Node **head = getHeadNode(key, hashMap);
+            //The head is of type Node ** because it represents a pointer to the start of the buffer, containing pointers to the nodes
+
+        // if there is no head node, make a pointer to the node that is made
+        if (*head == NULL) {
+            // head is dereferenced to check if the Node *, which head points to, points to a node
+            *head = currentNode;
+        }
+        // if there is a head node, use a while to traverse from node to node
+        else {
+            struct Node *current = *head;
+            while (current->next != NULL) {
+                current = current->next;
+            }
+            current->next = currentNode;
+        }
+        return 0;
+    }
+    return 1;
+}
+
+struct Node *getNode(int key, struct HashMap *hashMap) {
+    struct Node **head = getHeadNode(key, hashMap);
+    if (*head == NULL) {
+        return NULL;
+    }
+    struct Node *current = *head;
+    while (current->next != NULL) {
+        if (current->key == key) {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+struct Result getValue(int key, struct HashMap *hashMap) {
+    struct Result result;
+    struct Node *node = getNode(key, hashMap);
+    if (node == NULL) {
+        result.foundValue = false;
+        result.value = 0;
+        return result;
+    }
+    result.foundValue = true;
+    result.value = node->value;
+    return result;
+}
+
+int setValue(int key, int newVal, struct HashMap *hashMap) {
+    struct Node *node = getNode(key, hashMap);
+    if (node == NULL) {
+        return 1;
+    }
+    node->value = newVal;
+    return 0;
+}
+
+void printNodeByKey(int key, struct HashMap *hashMap) {
+    struct Node *head = getNode(key, hashMap);
+    if (head == NULL) {
+        printf("Null ");
+        return;
+    }
+    struct Node *current = head;
+    while (head->next != NULL) {
+        current = head->next;
+        if (current->key == key) {
+            break;
+        }
+    }
+    printNode(current);
+}
+
+void printNode(struct Node *node) {
+    printf("Address: %p, ", node);
+
+    printf("Key: %d, ", node->key);
+    printf("Value: %d, ", node->value);
+
+    printf("Next: %p \n", node->next);
+}
+
+void printHashMap(struct HashMap *hashmap) {
+    struct Node **heads = (struct Node **)hashmap->buffer;
+    printf("Number of buckets: %d", hashmap->numBuckets);
+    for (int i = 0; i < hashmap->numBuckets; i++) {
+        printf("Pointer: %p \n", *(heads + i));
+    }
+}
+
+void printBucket(struct HashMap *hashmap, int bucketNum) {
+    struct Node **head = hashmap->buffer + bucketNum;
+    struct Node *current = *head;
+    while (current->next != NULL) {
+        printNode(current);
+        current = current->next;
+    }
+}
+
+struct Node **getHeadNode(int key, struct HashMap *hashMap) {
+    struct Node ** buff = (struct Node **)(hashMap->buffer);
+    int offset = key % (hashMap->numBuckets);
+    return buff + offset;
+}
---
+++ main.h
@@ -0,0 +1,37 @@
+//
+// Created by yuanx on 5/3/2026.
+//
+
+#include <stdbool.h>
+
+struct Node {
+    struct Node *next;
+    int key;
+    int value;
+} ;
+
+struct HashMap {
+    void *buffer;
+    size_t size;
+    struct Node *unalloc;
+    int numNodes;
+    int numBuckets;
+};
+
+struct Result {
+    int value;
+    bool foundValue;
+};
+
+struct Node **getHeadNode(int key, struct HashMap *hashMap);
+struct HashMap makeHashMap(int num);
+int addNode(int key, int val, struct HashMap *hashMap);
+struct Result getValue(int key, struct HashMap *hashMap);
+void printNode(struct Node *node);
+void printNodeByKey(int key, struct HashMap *hashMap);
+void printHashMap(struct HashMap *hashmap);
+void printBucket(struct HashMap *hashmap, int bucketNum);
+#ifndef HASHMAP_MAIN_H
+#define HASHMAP_MAIN_H
+
+#endif //HASHMAP_MAIN_H
